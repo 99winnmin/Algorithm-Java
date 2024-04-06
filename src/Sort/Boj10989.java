26992bb3835c5b009b5bf7952c456425aa3cb555
99winnmin <ojysep9987@gmail.com>
2024-04-06 14:40:06 +0900

    :sparkles: 카운트 정렬

---
+++ src/Sort/Boj10989.java
@@ -0,0 +1,25 @@
+package Sort;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj10989 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int[] arr = new int[10001];
+        for(int i=0 ; i<N ; i++) {
+            arr[Integer.parseInt(br.readLine())]++;
+        }
+
+        for(int i=0 ; i<10001 ; i++) {
+            if (arr[i] > 0) {
+                sb.append(i).append("\n");
+                arr[i]--;
+            }
+        }
+        System.out.println(sb);
+    }
+}
